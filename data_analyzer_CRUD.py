ce1dfb00cb4b556e20fc0371e7ace94267c757e9
isa-bay <isa.bayramov.2903@mail.ru>
2025-10-12 14:10:46 +0300

    Добавил файл анализа crus операций

---
+++ data_analyzer_CRUD.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+data = pd.read_csv("operations.csv")
+
+print(data["operation"].value_count())
+
+print(data.groupby("operation")["duration_ms"].mean().round(2))
+
+print(data.groupby("table_name")["operation"].count())
+
+print(data.groupby("user_id")["operation"].count().sort_values(ascending=False))
